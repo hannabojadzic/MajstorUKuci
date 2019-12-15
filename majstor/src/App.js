bb6a0958ba119397e543d2304f7a856735243046
hannabojadzic <hbojadzic1@etf.unsa.ba>
2019-12-16 00:46:41 +0100

    First commit

---
+++ majstor/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pricing from './Pricing'
+
+function App() {
+  return (
+    <div className="App">
+     <Pricing></Pricing>
+    </div>
+  );
+}
+
+export default App;
